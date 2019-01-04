d99461445f7065ce19cb80c33326418808066b3a
andywu0489 <andywu0489@gmail.com>
2019-01-04 15:11:03 -0500

    Add code to display X or O on game board click
    Added functions for turn logic and to display X and O on game board
      click in events.js
    
    Added divs in index.html to append the X and O to

--- assets/scripts/tic-tac-toe-logic/events.js
+++ assets/scripts/tic-tac-toe-logic/events.js
@@ -1,30 +1,86 @@
+const turns = ['']
+
 const onBoxOneClick = (event) => {
   console.log('1 clicked')
+  turns.push('one')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#one')
+  } else {
+    $('<p>O</p>').appendTo('#one')
+  }
 }
 
 const onBoxTwoClick = (event) => {
   console.log('2 clicked')
+  turns.push('two')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#two')
+  } else {
+    $('<p>O</p>').appendTo('#two')
+  }
 }
 const onBoxThreeClick = (event) => {
   console.log('3 clicked')
+  turns.push('three')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#three')
+  } else {
+    $('<p>O</p>').appendTo('#three')
+  }
 }
 const onBoxFourClick = (event) => {
   console.log('4 clicked')
+  turns.push('four')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#four')
+  } else {
+    $('<p>O</p>').appendTo('#four')
+  }
 }
 const onBoxFiveClick = (event) => {
   console.log('5 clicked')
+  turns.push('five')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#five')
+  } else {
+    $('<p>O</p>').appendTo('#five')
+  }
 }
 const onBoxSixClick = (event) => {
   console.log('6 clicked')
+  turns.push('six')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#six')
+  } else {
+    $('<p>O</p>').appendTo('#six')
+  }
 }
 const onBoxSevenClick = (event) => {
   console.log('7 clicked')
+  turns.push('seven')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#seven')
+  } else {
+    $('<p>O</p>').appendTo('#seven')
+  }
 }
 const onBoxEightClick = (event) => {
   console.log('8 clicked')
+  turns.push('eight')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#eight')
+  } else {
+    $('<p>O</p>').appendTo('#eight')
+  }
 }
 const onBoxNineClick = (event) => {
   console.log('9 clicked')
+  turns.push('nine')
+  if (turns.length % 2 === 0) {
+    $('<p>X</p>').appendTo('#nine')
+  } else {
+    $('<p>O</p>').appendTo('#nine')
+  }
 }
 
 module.exports = {
