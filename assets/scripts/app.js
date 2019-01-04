928bb20689b6c2be3d2547bd61bf1928078e52cb
andywu0489 <andywu0489@gmail.com>
2019-01-04 13:58:17 -0500

    Begin game UI
    -Designed game board
    -Added click handlers for when space on board is clicked

--- assets/scripts/app.js
+++ assets/scripts/app.js
@@ -2,10 +2,18 @@
 
 // use require with a reference to bundle the file and use it in this file
 // const example = require('./example')
-
+const events = require('./tic-tac-toe-logic/events')
 // use require without a reference to ensure a file is bundled
 // require('./example')
 
 $(() => {
-  // your JS code goes here
+  $('#box-1').on('click', events.onBoxOneClick)
+  $('#box-2').on('click', events.onBoxTwoClick)
+  $('#box-3').on('click', events.onBoxThreeClick)
+  $('#box-4').on('click', events.onBoxFourClick)
+  $('#box-5').on('click', events.onBoxFiveClick)
+  $('#box-6').on('click', events.onBoxSixClick)
+  $('#box-7').on('click', events.onBoxSevenClick)
+  $('#box-8').on('click', events.onBoxEightClick)
+  $('#box-9').on('click', events.onBoxNineClick)
 })
---
+++ assets/scripts/tic-tac-toe-logic/events.js
@@ -0,0 +1,40 @@
+const onBoxOneClick = (event) => {
+  console.log('1 clicked')
+}
+
+const onBoxTwoClick = (event) => {
+  console.log('2 clicked')
+}
+const onBoxThreeClick = (event) => {
+  console.log('3 clicked')
+}
+const onBoxFourClick = (event) => {
+  console.log('4 clicked')
+}
+const onBoxFiveClick = (event) => {
+  console.log('5 clicked')
+}
+const onBoxSixClick = (event) => {
+  console.log('6 clicked')
+}
+const onBoxSevenClick = (event) => {
+  console.log('7 clicked')
+}
+const onBoxEightClick = (event) => {
+  console.log('8 clicked')
+}
+const onBoxNineClick = (event) => {
+  console.log('9 clicked')
+}
+
+module.exports = {
+  onBoxOneClick,
+  onBoxTwoClick,
+  onBoxThreeClick,
+  onBoxFourClick,
+  onBoxFiveClick,
+  onBoxSixClick,
+  onBoxSevenClick,
+  onBoxEightClick,
+  onBoxNineClick
+}
