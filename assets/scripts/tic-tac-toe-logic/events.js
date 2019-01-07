2aee5753e5ebf9ae61587ee4b40f8e9d20430641
andywu0489 <andywu0489@gmail.com>
2019-01-07 12:35:55 -0500

    Add game logic to determine win
    -Added win function to events.js
    -Added winText div to index.html for message upon win
    -Added style to winText div in index.scss

--- assets/scripts/tic-tac-toe-logic/events.js
+++ assets/scripts/tic-tac-toe-logic/events.js
@@ -1,147 +1,236 @@
-const turns = ['']
+const turns = []
+const board = ['', '', '', '', '', '', '', '', '']
+
+const win = () => {
+  if (board[0] === 'x' && board[1] === 'x' && board[2] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[3] === 'x' && board[4] === 'x' && board[5] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[6] === 'x' && board[7] === 'x' && board[8] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[0] === 'x' && board[3] === 'x' && board[6] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[1] === 'x' && board[4] === 'x' && board[7] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[2] === 'x' && board[5] === 'x' && board[8] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[0] === 'x' && board[4] === 'x' && board[8] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[2] === 'x' && board[4] === 'x' && board[6] === 'x') {
+    console.log('X wins')
+    $('#winText').html('<p>Player X Wins!</p>')
+  } else if (board[0] === 'o' && board[1] === 'o' && board[2] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[3] === 'o' && board[4] === 'o' && board[5] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[6] === 'o' && board[7] === 'o' && board[8] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[0] === 'o' && board[3] === 'o' && board[6] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[1] === 'o' && board[4] === 'o' && board[7] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[2] === 'o' && board[5] === 'o' && board[8] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[0] === 'o' && board[4] === 'o' && board[8] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  } else if (board[2] === 'o' && board[4] === 'o' && board[6] === 'o') {
+    console.log('O wins')
+    $('#winText').html('<p>Player O Wins!</p>')
+  }
+}
 
 if ((turns.length + 1) % 2 === 0) {
-  console.log(turns)
-  $('#turn').html('<p>Player X turn</p>')
-} else if ((turns.length + 1) % 2 === 1) {
   $('#turn').html('<p>Player O turn</p>')
+} else if ((turns.length + 1) % 2 === 1) {
+  $('#turn').html('<p>Player X turn</p>')
 }
 
 const onBoxOneClick = (event) => {
-  console.log('1 clicked')
-  turns.push('one')
-  if (turns.length % 2 === 0) {
+  turns.push(1)
+  console.log(turns)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#one')
   } else {
     $('<p>O</p>').appendTo('#one')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[0] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[0] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 
 const onBoxTwoClick = (event) => {
-  console.log('2 clicked')
-  turns.push('two')
-  if (turns.length % 2 === 0) {
+  turns.push(2)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#two')
   } else {
     $('<p>O</p>').appendTo('#two')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[1] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[1] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxThreeClick = (event) => {
-  console.log('3 clicked')
-  turns.push('three')
-  if (turns.length % 2 === 0) {
+  turns.push(3)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#three')
   } else {
     $('<p>O</p>').appendTo('#three')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[2] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[2] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxFourClick = (event) => {
-  console.log('4 clicked')
-  turns.push('four')
-  if (turns.length % 2 === 0) {
+  turns.push(4)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#four')
   } else {
     $('<p>O</p>').appendTo('#four')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[3] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[3] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxFiveClick = (event) => {
-  console.log('5 clicked')
-  turns.push('five')
-  if (turns.length % 2 === 0) {
+  turns.push(5)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#five')
   } else {
     $('<p>O</p>').appendTo('#five')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[4] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[4] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxSixClick = (event) => {
-  console.log('6 clicked')
-  turns.push('six')
-  if (turns.length % 2 === 0) {
+  turns.push(6)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#six')
   } else {
     $('<p>O</p>').appendTo('#six')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[5] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[5] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxSevenClick = (event) => {
-  console.log('7 clicked')
-  turns.push('seven')
-  if (turns.length % 2 === 0) {
+  turns.push(7)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#seven')
   } else {
     $('<p>O</p>').appendTo('#seven')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[6] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[6] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxEightClick = (event) => {
-  console.log('8 clicked')
-  turns.push('eight')
-  if (turns.length % 2 === 0) {
+  turns.push(8)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#eight')
   } else {
     $('<p>O</p>').appendTo('#eight')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[7] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[7] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 const onBoxNineClick = (event) => {
-  console.log('9 clicked')
-  turns.push('nine')
-  if (turns.length % 2 === 0) {
+  turns.push(9)
+  if (turns.length % 2 === 1) {
     $('<p>X</p>').appendTo('#nine')
   } else {
     $('<p>O</p>').appendTo('#nine')
   }
-  if ((turns.length + 1) % 2 === 0) {
-    console.log(turns)
+  if ((turns.length + 1) % 2 === 1) {
     $('#turn').html('<p>Player X turn</p>')
-  } else if ((turns.length + 1) % 2 === 1) {
+    board[8] = 'o'
+    console.log(board)
+  } else if ((turns.length + 1) % 2 === 0) {
     $('#turn').html('<p>Player O turn</p>')
+    board[8] = 'x'
+    console.log(board)
   }
+  console.log(turns)
+  win()
 }
 
 module.exports = {
