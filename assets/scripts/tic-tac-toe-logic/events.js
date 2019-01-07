6b7136968ecfa0908386c6232a2590d42e66fc87
andywu0489 <andywu0489@gmail.com>
2019-01-07 13:01:38 -0500

    Add code to disable clicks after win and show message if clicked

--- assets/scripts/tic-tac-toe-logic/events.js
+++ assets/scripts/tic-tac-toe-logic/events.js
@@ -1,55 +1,115 @@
 const turns = []
 const board = ['', '', '', '', '', '', '', '', '']
 
+const disableClicks = () => {
+  $('#box-1').off('click')
+  $('#box-2').off('click')
+  $('#box-3').off('click')
+  $('#box-4').off('click')
+  $('#box-5').off('click')
+  $('#box-6').off('click')
+  $('#box-7').off('click')
+  $('#box-8').off('click')
+  $('#box-9').off('click')
+}
+
+const onInvalidBoxClick = (event) => {
+  $('#winText').html('<p>Invalid Move!</p>')
+}
+
+const invalidMessage = () => {
+  $('#box-1').on('click', onInvalidBoxClick)
+  $('#box-2').on('click', onInvalidBoxClick)
+  $('#box-3').on('click', onInvalidBoxClick)
+  $('#box-4').on('click', onInvalidBoxClick)
+  $('#box-5').on('click', onInvalidBoxClick)
+  $('#box-6').on('click', onInvalidBoxClick)
+  $('#box-7').on('click', onInvalidBoxClick)
+  $('#box-8').on('click', onInvalidBoxClick)
+  $('#box-9').on('click', onInvalidBoxClick)
+}
+
 const win = () => {
   if (board[0] === 'x' && board[1] === 'x' && board[2] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[3] === 'x' && board[4] === 'x' && board[5] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[6] === 'x' && board[7] === 'x' && board[8] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[0] === 'x' && board[3] === 'x' && board[6] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[1] === 'x' && board[4] === 'x' && board[7] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[2] === 'x' && board[5] === 'x' && board[8] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[0] === 'x' && board[4] === 'x' && board[8] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[2] === 'x' && board[4] === 'x' && board[6] === 'x') {
     console.log('X wins')
     $('#winText').html('<p>Player X Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[0] === 'o' && board[1] === 'o' && board[2] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[3] === 'o' && board[4] === 'o' && board[5] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[6] === 'o' && board[7] === 'o' && board[8] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[0] === 'o' && board[3] === 'o' && board[6] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[1] === 'o' && board[4] === 'o' && board[7] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[2] === 'o' && board[5] === 'o' && board[8] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[0] === 'o' && board[4] === 'o' && board[8] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   } else if (board[2] === 'o' && board[4] === 'o' && board[6] === 'o') {
     console.log('O wins')
     $('#winText').html('<p>Player O Wins!</p>')
+    disableClicks()
+    invalidMessage()
   }
 }
 
